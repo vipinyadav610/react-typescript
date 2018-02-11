b6a6c004eb455a3586a36d0fdba5acc92dbda888
vipinyadav610 <adi19910810@gmail.com>
2018-02-11 08:52:13 +0530

    babel setting change

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 
 export interface AppProps { compiler: string; framework: string; }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export class App extends React.Component<AppProps, {}> {
+export default class App extends React.Component<AppProps, {}> {
     render() {
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
